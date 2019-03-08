92ae5f8ee55238f42665d6e7e8840c3f46755003
Gunnar Digranes <gudi@equinor.com>
2019-03-08 15:04:55 +0100

    kkj

--- client.py
+++ client.py
@@ -5,5 +5,19 @@ import githubapimock as api
 config_path = os.path.expanduser("~/repo.yml")
 settings = yaml.load(open(config_path))
 
-print(settings)
+org = settings["org"]
+repo = settings["repo"]
+user = settings["user"]
+token = settings["token"]
+
+title = ""
+while title != "q":
+   title = input("Title: ")
+   body = input("Body: ")
+   label = input("Label: ")
+   labels = [label]
+   
+   issue_id = api.create_issue(org, repo, user, token, title, body)
+
+
 
